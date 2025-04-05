89ed0066616b870ab03a33adc40bb0fbc692f254
Guillermo Valdes <guillermo@movimientolibre.com>
2025-04-05 09:59:35 -0600

    Disable else

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
     sys.exit(0)
-else:
+# else:
     # Run the FastAPI app with gunicorn server ASGI for production
-    port = int(os.environ.get("PORT", 8080))
-    worker_class = os.environ.get("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
-    workers = int(os.environ.get("GUNICORN_WORKERS", 1))
-    cmd = f"gunicorn --bind 0.0.0.0:{port} --workers {workers} --threads 8 --timeout 0 -k {worker_class} pjecz_centauro_api_key.main:app"
-    os.system(cmd)
-    sys.exit(0)
+    # port = int(os.environ.get("PORT", 8080))
+    # worker_class = os.environ.get("WORKER_CLASS", "uvicorn.workers.UvicornWorker")
+    # workers = int(os.environ.get("GUNICORN_WORKERS", 1))
+    # cmd = f"gunicorn --bind 0.0.0.0:{port} --workers {workers} --threads 8 --timeout 0 -k {worker_class} pjecz_centauro_api_key.main:app"
+    # os.system(cmd)
+    # sys.exit(0)
